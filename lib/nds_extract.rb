f29595f93ff837cd8df26281949ac11c719194fa
Matt Bechtel <bechma02@gmail.com>
2019-10-21 20:05:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,7 +21,7 @@ def flatten_a_o_a(aoa)
 end
 
 def movie_with_director_name(director_name, movie_data)
-  { 
+  {
     :title => movie_data[:title],
     :worldwide_gross => movie_data[:worldwide_gross],
     :release_year => movie_data[:release_year],
@@ -76,7 +76,7 @@ def gross_per_studio(collection)
     end
 
     i += 1
-    
+
   end
 
   result
